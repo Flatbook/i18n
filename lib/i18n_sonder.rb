f4615fce8e1d2d7f9eaa04bea7ef6fe547801c7d
Girish Balakrishnan <gurug1990@gmail.com>
2020-05-11 16:22:34 -0700

    Remove unecessary require

--- lib/i18n_sonder.rb
+++ lib/i18n_sonder.rb
@@ -3,7 +3,6 @@ require "crowdin/client"
 require "mobility/backends/default_locale_optimized_key_value"
 require "mobility/plugins/locale_optimized_query"
 
-require "pp"
 module I18nSonder
   class Error < StandardError; end
 end
