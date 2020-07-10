00be14018b39630e3fb643edd59986ecb0236af5
Girish Balakrishnan <gurug1990@gmail.com>
2020-07-10 07:21:40 -0700

    Allow adding predicate for upload_for_translation Mobility plugin

--- lib/mobility/plugins/upload_for_translation.rb
+++ lib/mobility/plugins/upload_for_translation.rb
@@ -14,11 +14,7 @@ module Mobility
       UPLOAD_TRANSLATION_DELAY = (ENV["UPLOAD_FOR_TRANSLATION_DELAY_MIN"] || 5).minutes
 
       def write(locale, value, options = {})
-        old_value = model.read_attribute(attribute)
-
-        # Only upload for translation if we are writing content in the default locale,
-        # and if the new value being written is different to the already existing value.
-        if locale == I18n.default_locale && value != old_value && model[:id].present?
+        if should_upload_for_translation?(model)
           # Get translated attributes, and each attribute's params for uploading translations
           translated_attribute_names = model_class.translated_attribute_names
           translated_attribute_params = {}
@@ -35,6 +31,27 @@ module Mobility
         end
 
         super
+      end
+
+      private
+
+      # Only upload for translation if:
+      # 1) we are writing content in the default locale
+      # 2) the new value being written is different to the already existing value.
+      # 3) there is an ID present for the model
+      # 4) if any predicate was passed in for this model's translatable field, then that evaluates to true
+      def should_upload_for_translation?(model)
+        is_default_locale = locale == I18n.default_locale
+
+        old_value = model.read_attribute(attribute)
+        is_different_new_value = value != old_value
+
+        model_id_present = model[:id].present?
+
+        pred = model.public_send("#{attribute}_backend").options.dig(:upload_for_translation, :predicate)
+        eval_pred = pred.nil? || pred.call(model)
+
+        is_default_locale && is_different_new_value && model_id_present && eval_pred
       end
     end
   end
