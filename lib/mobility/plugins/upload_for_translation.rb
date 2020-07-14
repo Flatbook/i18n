10f6ca3f7d90114f2661d140b94b197ffe9cfdbc
Girish Balakrishnan <gurug1990@gmail.com>
2020-07-14 10:27:47 -0700

    Add tests

--- lib/mobility/plugins/upload_for_translation.rb
+++ lib/mobility/plugins/upload_for_translation.rb
@@ -14,7 +14,7 @@ module Mobility
       UPLOAD_TRANSLATION_DELAY = (ENV["UPLOAD_FOR_TRANSLATION_DELAY_MIN"] || 5).minutes
 
       def write(locale, value, options = {})
-        if should_upload_for_translation?(model)
+        if should_upload_for_translation?(model, locale, value, attribute)
           # Get translated attributes, and each attribute's params for uploading translations
           translated_attribute_names = model_class.translated_attribute_names
           translated_attribute_params = {}
@@ -39,8 +39,8 @@ module Mobility
       # 1) we are writing content in the default locale
       # 2) the new value being written is different to the already existing value.
       # 3) there is an ID present for the model
-      # 4) if any predicate was passed in for this model's translatable field, then that evaluates to true
-      def should_upload_for_translation?(model)
+      # 4) if this model is allowed for translation
+      def should_upload_for_translation?(model, locale, value, attribute)
         is_default_locale = locale == I18n.default_locale
 
         old_value = model.read_attribute(attribute)
@@ -48,10 +48,11 @@ module Mobility
 
         model_id_present = model[:id].present?
 
-        pred = model.public_send("#{attribute}_backend").options.dig(:upload_for_translation, :predicate)
-        eval_pred = pred.nil? || pred.call(model)
+        # Check if model has the method defined and if it evaluates to true
+        model_allowed_for_translation = !model.class.method_defined?(:allowed_for_translation?) ||
+            model.allowed_for_translation?
 
-        is_default_locale && is_different_new_value && model_id_present && eval_pred
+        is_default_locale && is_different_new_value && model_id_present && model_allowed_for_translation
       end
     end
   end
