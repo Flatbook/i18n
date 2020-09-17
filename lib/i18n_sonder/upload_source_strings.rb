9a67c3b4a86e4d0b09d2a4ac7feee1233cd5f505
Alexander Fong <alpfong@gmail.com>
2020-09-17 09:56:04 -0700

    Move shared upload logic to UploadSourceStrings

--- lib/i18n_sonder/upload_source_strings.rb
+++ lib/i18n_sonder/upload_source_strings.rb
@@ -1,11 +1,9 @@
 module I18nSonder
   class UploadSourceStrings
-    include UploadHelpers
+    UPLOAD_TRANSLATION_DELAY = (ENV["UPLOAD_FOR_TRANSLATION_DELAY_MIN"] || 5).minutes
 
-    def perform(model, options = {})
-      # TOOD: Don't need to check if shoud_upload_for_translation?
-      # since most of the time can assume this is triggered when a unit is first added to
-      # return unless should_upload_for_translation?(model, locale, value, attribute)
+    def self.upload(model, locale, value, attribute, options = {})
+      return unless should_upload_for_translation?(model, locale, value, attribute)
 
       # Asynchronously upload attributes for translations
       # Include a delay so that multiple edits to the same object can be 'de-duped' in the async job.
@@ -18,6 +16,46 @@ module I18nSonder
           namespace: namespace(model)
         }
       )
+    end
+
+    private
+
+    def self.get_translated_attribute_params(model)
+      # Get translated attributes and each attribute's params for uploading translations
+      translated_attribute_names = model.translated_attribute_names
+      translated_attribute_params = {}
+      translated_attribute_names.each do |attribute_name|
+        upload_options = model.public_send("#{attribute_name}_backend").options[:upload_for_translation]
+        translated_attribute_params[attribute_name] = upload_options.is_a?(Hash) ? upload_options : {}
+      end
+
+      translated_attribute_params
+    end
+
+    def self.namespace(model)
+      if model.class.method_defined?(:namespace_for_translation)
+        model.namespace_for_translation
+      end
+    end
+
+    # Only upload for translation if:
+    # 1) we are writing content in the default locale
+    # 2) the new value being written is different to the already existing value.
+    # 3) there is an ID present for the model
+    # 4) if this model is allowed for translation
+    def self.should_upload_for_translation?(model, locale, value, attribute)
+      is_default_locale = locale == I18n.default_locale
+
+      old_value = model.read_attribute(attribute)
+      is_different_new_value = value != old_value
+
+      model_id_present = model[:id].present?
+
+      # Check if model has the method defined and if it evaluates to true
+      model_allowed_for_translation = !model.class.method_defined?(:allowed_for_translation?) ||
+          model.allowed_for_translation?
+
+      is_default_locale && is_different_new_value && model_id_present && model_allowed_for_translation
     end
   end
 end
--- lib/mobility/plugins/upload_for_translation.rb
+++ lib/mobility/plugins/upload_for_translation.rb
@@ -1,11 +1,9 @@
 require 'mobility'
-require 'i18n_sonder/upload_helpers'
+require 'i18n_sonder/upload_source_strings'
 
 module Mobility
   module Plugins
     module UploadForTranslation
-      include I18nSonder::UploadHelpers
-
       class << self
         def apply(attributes, option)
           return unless option
@@ -15,19 +13,7 @@ module Mobility
       end
 
       def write(locale, value, options = {})
-        return unless should_upload_for_translation?(model, locale, value, attribute)
-
-        # Asynchronously upload attributes for translations
-        # Include a delay so that multiple edits to the same object can be 'de-duped' in the async job.
-        I18nSonder::Workers::UploadSourceStringsWorker.perform_in(
-            UPLOAD_TRANSLATION_DELAY,
-            model.class.name,
-            model[:id],
-            {
-              translated_attribute_params: get_translated_attribute_params(model),
-              namespace: namespace(model)
-            }
-        )
+        I18nSonder::UploadSourceStrings.upload(model, locale, value, attribute)
 
         super
       end
